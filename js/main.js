0efd5bb8a4b3ce2e3c163e900f33a339fab39ee2
Le Duc Duy <duy@dwork.vn>
2017-05-04 16:19:35 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+  $(document).ready(function() {
+
+  wow = new WOW({
+        boxClass: 'wow',
+        animateClass: 'animated', 
+        offset: 0, 
+        mobile: true, 
+        live: true 
+    })
+    wow.init();
+
+  });
+
+	
+
+	 
+
+
